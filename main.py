04f0f3f287c21cb9ffe3b3b33ca0648b4865da8e
generocket <generocket@github.com>
2024-04-10 00:11:31 +0800

    优化代码

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import os
+
 from scrapy import cmdline
+
+if not os.path.exists("byr_data.json"):
+    with open("byr_data.json", "a+") as _:
+        pass
+
+if not os.path.exists("byr_data_merge.json"):
+    with open("byr_data_merge.json", "a+") as _:
+        pass
 
 cmdline.execute("scrapy crawl board_spider".split())
